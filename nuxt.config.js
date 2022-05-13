8aaceef000388b58df14874b21c982e620c8aad9
Leonardo Panseri <leonardo.panseri@gmail.com>
2022-05-13 13:40:09 +0200

    Update template

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,7 +51,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'https://lambrate-hypermedia.herokuapp.com/'
+    baseURL: process.env.NODE_ENV === 'production' ? 'https://lambrate-hypermedia.herokuapp.com/' : 'http://localhost:3000'
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
