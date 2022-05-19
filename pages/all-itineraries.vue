826cab9e5fca1f51a6c51fdbb6116f8c886fad83
amangabba <aman.gabba@gmail.com>
2022-05-19 15:56:30 +0200

    Integrated all-itineraries.vue with IntroductoryPage.vue; deleted all_events.vue + CardItinerary.vue

--- pages/all-itineraries.vue
+++ pages/all-itineraries.vue
@@ -1,38 +1,40 @@
 <template>
-    <div class="page container mt-5 text-center">
-        <h1 class="display-4">All Itineraries</h1>
-        <div class="row mt-3 justify-content-center">
-            <CardItinerary
-                v-for="itinerary of itineraryList"
-                :id="itinerary.id"
-                :key="itinerary.id"
-                class="col-sm-2 m-2 img-thumbnail"
-                :title="itinerary.title"
-                :duration="itinerary.duration"
-                :description="itinerary.description"
-                :map_link="itinerary.map_link"
-                :poi_list="itinerary.poi_list"
-            />
-        </div>
-    </div>
+    <IntroductoryPage
+        :title="title"
+        :element-list="itineraryList"
+        :cover-image-link="coverImageLink"
+        :subtitle="subtitle" />
 </template>
 
 <script>
-// import CardItinerary from '~/components/CardItinerary'
+import IntroductoryPage from "~/components/IntroductoryPage";
 export default {
     name: 'ItinerariesPage',
-    components: {
-        //    CardItinerary,
-    },
+    components: { IntroductoryPage },
 
     async asyncData({ $axios }) {
         const { data } = await $axios.get('/api/itineraries')
+        const itineraryList = []
+        for (const elem of data) {
+            const poiList = elem.poi_list
+            const description = poiList.length ? 'From: ' + poiList[0].name + ' ' + poiList[poiList.length - 1].name + ' | ' : ''
+            itineraryList.push({
+                title: elem.title,
+                imageLink: elem.map_link,
+                link: `/itineraries/${elem.id}`,
+                description: description + 'Duration: ' + elem.duration
+            })
+        }
         return {
-            itineraryList: data
+            itineraryList
         }
     },
     data() {
-        return {}
+        return {
+            title:"All Itineraries",
+            subtitle:"Visit the city with different plans!",
+            coverImageLink: "https://wallpaperaccess.com/full/2667331.jpg",
+        }
     }
 }
 </script>
