0009c72cd3e074ca9885a7d3b10591411bca4d83
amangabba <aman.gabba@gmail.com>
2022-05-25 19:18:30 +0200

    Carousel with multiple elements

--- pages/pois/_id.vue
+++ pages/pois/_id.vue
@@ -10,10 +10,22 @@
             <p>{{ description }}</p>
             <img class="w-50 mx-auto" :src="image_links[0]" alt="Image" />
             <img class="w-50 mx-auto" :src="image_links[1]" alt="Image" />
+            <img class="w-50 mx-auto" :src="image_links[2]" alt="Image" />
             <br />
         </div>
         <h3 class="text-left">See correlated Points of Interest:</h3>
         <carousel-component
+            :id="'itineraries'"
+            class="w-25 mx-auto text-left"
+            :cover-image-links="image_links"
+        ></carousel-component>
+        <carousel-component
+            :id="'pois'"
+            class="w-25 mx-auto text-left"
+            :cover-image-links="image_links"
+        ></carousel-component>
+        <carousel-component
+            :id="'events'"
             class="w-25 mx-auto text-left"
             :cover-image-links="image_links"
         ></carousel-component>
