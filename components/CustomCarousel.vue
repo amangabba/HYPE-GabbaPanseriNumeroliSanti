8e7b25ff32251f0c5bd6d257a39be57287487690
RITA0109 <rita.numeroli@gmail.com>
2022-05-25 12:11:36 +0200

    Aggiunta informazioni sugli eventi

--- components/CustomCarousel.vue
+++ components/CustomCarousel.vue
@@ -7,15 +7,15 @@
         <div class="carousel-inner">
             <div class="carousel-item active" data-bs-interval="10000">
                 <img :src="coverImageLinks[0]" class="d-block w-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
+              <!-- <div class="carousel-caption d-none d-md-block">
                     <h5>Previous</h5>
-                </div>
+                </div> --> 
             </div>
             <div class="carousel-item" data-bs-interval="2000">
                 <img :src="coverImageLinks[1]" class="d-block w-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
+               <!--  <div class="carousel-caption d-none d-md-block">
                     <h5>Next</h5>
-                </div>
+                </div> --> 
             </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
--- pages/events/_id.vue
+++ pages/events/_id.vue
@@ -1,31 +1,39 @@
 <template>
     <div class="justify-content-center container-fluid">
         <div class="title-row row p-5 text-center bg-primary bg-opacity-50 mb-5">
-            <h6 class="display-4" >{{ name }}</h6>
+            <h6 class="display-4">{{ name }}</h6>
         </div>
         <div class="row m-2">
-            <img class="col-md-6 h-auto text-center" :src="image_links[0]" alt="Image" />
+            <carousel-component class="w-25 mx-auto text-left" :cover-image-links="image_links"></carousel-component>
+            <!--<img class="col-md-6 h-auto text-center" :src="image_links[0]" alt="Image" /> -->
             <div class="col-md-6 text-left p-2">
-            <b>{{ practical_info }}</b> <!-- in che senso? io non le metterei--->
-            <p>The event starts on: {{ start_date }}</p>
-            <p>The event ends on: {{ end_date }}</p>
-            <p> Address: {{ address }}</p>
-            <p> Location: (link of a poi) </p> <!--link of a poi-->
-            <p>{{ season }}</p>   
-            </div>       
+                <font face="Arial" size="4px">
+                    <p>{{ practical_info }}</p> <!-- in che senso? io non le metterei--->
+                    <p> <i> The event starts on </i>: {{ start_date }}</p>
+                    <p> <i> The event ends on </i>: {{ end_date }}</p>
+                    <p> <i> Address </i>: {{ address }}</p>
+                    <p> <i> Location </i>: <a href="/pois/5"> Palazzina di Caccia Stupinigi </a></p>
+                    <!--link of a poi, ma non è dinamico cosi-->
+                    <p>{{ season }}</p>
+                </font>
+            </div>
         </div>
         <div class="row justify-content-center mx-auto mt-2">
-        <p> {{ description }} </p>
+            <font face="Arial" size="4px">
+                <p> {{ description }} </p>
+            </font>
         </div>
         <div class="row justify-content-center mx-auto mt-2">
-        <p> Other event in this location </p>
+            <p> Other event in this location </p>
         </div>
     </div>
 </template>
 
 <script>
+import CarouselComponent from '~/components/CustomCarousel'
 export default {
     name: 'EventPage',
+    components: {CarouselComponent},
     async asyncData({ route, $axios }) {
         const { id } = route.params
         const { data } = await $axios.get('/api/events/' + id)
@@ -46,4 +54,5 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+</style>
