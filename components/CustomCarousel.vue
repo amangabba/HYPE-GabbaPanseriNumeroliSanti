0e0d82f8829e77ad126bf2eafc6663259370ea36
Leonardo <leonardo.panseri@gmail.com>
2022-05-26 09:59:53 +0200

    Refactor itinerary multiple topic page

--- components/CustomCarousel.vue
+++ components/CustomCarousel.vue
@@ -1,28 +1,25 @@
 <template>
-    <div
-        :id="id"
-        class="carousel carousel-dark slide"
-        data-bs-ride="carousel"
-    >
+    <div :id="id" class="carousel carousel-dark slide" data-bs-ride="carousel">
         <div class="carousel-indicators">
             <button
                 v-for="index of Array(coverImageLinks.length).keys()"
-                :key="'button-'+index"
+                :key="'button-' + index"
                 type="button"
-                :data-bs-target="'#'+id"
+                :data-bs-target="'#' + id"
                 :data-bs-slide-to="index"
                 class="active"
                 aria-current="true"
-                :aria-label="'Slide '+index"
+                :aria-label="'Slide ' + index"
             ></button>
         </div>
         <div class="carousel-inner">
-            <div v-for="(img,index) of coverImageLinks" :key="'carousel-'+index" :class="index==0 ? 'carousel-item active' : 'carousel-item'" data-bs-interval="10000">
-                <img
-                    :src="img"
-                    class="d-block w-100"
-                    alt="..."
-                />
+            <div
+                v-for="(img, index) of coverImageLinks"
+                :key="'carousel-' + index"
+                :class="index === 0 ? 'carousel-item active' : 'carousel-item'"
+                data-bs-interval="10000"
+            >
+                <img :src="img" class="d-block w-100" alt="..." />
                 <div class="carousel-caption d-none d-md-block">
                     <h5>Previous</h5>
                 </div>
@@ -31,7 +28,7 @@
         <button
             class="carousel-control-prev"
             type="button"
-            :data-bs-target="'#'+id"
+            :data-bs-target="'#' + id"
             data-bs-slide="prev"
         >
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -40,7 +37,7 @@
         <button
             class="carousel-control-next"
             type="button"
-            :data-bs-target="'#'+id"
+            :data-bs-target="'#' + id"
             data-bs-slide="next"
         >
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
@@ -57,9 +54,9 @@ export default {
             type: Array,
             required: true
         },
-        id:{
+        id: {
             type: String,
-            required: true,
+            required: true
         }
     }
 }
