0ed84772555ff0caa14b6407b688e46d5c0097a3
MatteoSanti-prog <10665164@polimi.it>
2022-06-27 19:11:42 +0200

    Implementation of reach-the-city.vue

--- components/TravelInfoButtons.vue
+++ components/TravelInfoButtons.vue
@@ -1,9 +1,14 @@
 <template>
-    <div class="row m-2 text-center">
-        <div v-for="(element, elementIndex) of travelInfoList"
-             :key="`navItem${elementIndex}`"
-             class="col-md-4">
-            <a class="btn btn-primary mx-4 p-2" :href="element.path">{{ element.name }}</a>
+    <div class="mx-4 justify-content-center">
+        <div class="row bg-primary bg-opacity-10 p-2 text-center mt-2 mb-2">
+            <h2 class="display-5">Correlated Topics</h2>
+        </div>
+        <div class="row m-2 text-center">
+            <div v-for="(element, elementIndex) of travelInfoList"
+                 :key="`navItem${elementIndex}`"
+                 class="col-md-4">
+                <a class="btn btn-primary mx-4 p-2" :href="element.path">{{ element.name }}</a>
+            </div>
         </div>
     </div>
 </template>
