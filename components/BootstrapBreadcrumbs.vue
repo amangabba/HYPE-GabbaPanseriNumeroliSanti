da926105c3a889205b95491bcdfd11518a348efe
amangabba <aman.gabba@gmail.com>
2022-06-30 18:50:17 +0200

    Possible breadcrumbs style

--- components/BootstrapBreadcrumbs.vue
+++ components/BootstrapBreadcrumbs.vue
@@ -4,14 +4,16 @@
             <li
                 v-for="(el, i) in elements"
                 :key="`breadcrumb${i}`"
-                :class="`breadcrumb-item${
+                :class="`p-2 breadcrumb-item${
                     i === elements.length - 1 ? ' active' : ''
                 }`"
             >
                 <!-- Show a link to the page if this is not the current page (last element in the array) -->
-                <NuxtLink v-if="i !== elements.length - 1" :to="el.link">{{
-                    el.title
-                }}</NuxtLink>
+                <NuxtLink
+                    v-if="i !== elements.length - 1"
+                    class="breadcrumb-link"
+                    :to="el.link">{{ el.title}}
+                </NuxtLink>
                 <span v-else>{{ el.title }}</span>
             </li>
         </ol>
@@ -37,7 +39,19 @@ export default {
 </script>
 
 <style scoped>
+
 nav {
     --bs-breadcrumb-divider: '>';
+}
+/* Da togliere se non convince */
+.breadcrumb-link {
+    color: black;
+    padding: 2px 5px 2px 5px;
+    border-radius: 3px;
+    text-decoration: none;
+    background-color: rgba(0,0,0, 0.2);
+}
+.breadcrumb-link:hover {
+    background-color: rgba(0,0,0, 0.35);
 }
 </style>
