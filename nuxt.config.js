43392e128749ae3bf32e06c163f46a9fba8469da
Leonardo Panseri <leonardo.panseri@gmail.com>
2022-07-03 16:21:11 +0200

    Defer bootstrap js load

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,8 @@ export default {
         link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
         script: [
             {
-                src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js'
+                src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js',
+                body: true
             }
         ]
     },
